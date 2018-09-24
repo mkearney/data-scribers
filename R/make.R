cc0feada2d9665ba5b3af1e1fb4636363e61e097
kmw <kearneymw@missouri.edu>
2018-09-24 13:14:20 -0500

    add R/make.R for cronjob

---
+++ R/make.R
@@ -0,0 +1,4 @@
+
+source("R/setup.R")
+source("R/read.R")
+source("R/write.R")
--- R/test.R
+++
@@ -1,5 +0,0 @@
-## check dir
-if (!file.exists("data-scribers.Rproj")) {
-  stop("You're in the wrong working directory!")
-}
-stop("this works")
